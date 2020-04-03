77834c8b507b72036accbf8e8374bc3a8dad075b
pgregory <pdlaxin@gmail.com>
2020-04-03 06:50:02 +0200

    added main terms for comparing to total

--- simulation/testing/mixed_terms_check.py
+++ simulation/testing/mixed_terms_check.py
@@ -7,7 +7,7 @@ import seaborn
 size = 300
 rows = 300
 cols = 300
-bins = 50
+bins = 100
 H = np.random.randn(rows, cols) / \
     np.sqrt(rows)@np.diag(np.exp(1j*np.random.uniform(0, 2*np.pi, rows)))@np.random.randn(rows, cols) / np.sqrt(rows)
 G = np.random.randn(rows, cols) / np.sqrt(rows)
@@ -17,8 +17,10 @@ HG = H @ np.conj(G.T)
 HH = H @ np.conj(H.T)
 total = GH + HG + HH + GG
 cross = GH + HG
+main = HH + GG
 GG_AED, bins_gg = np.histogram(np.linalg.eig(GG)[0], bins=bins)
 HH_AED, bins_hh = np.histogram(np.linalg.eig(HH)[0], bins=bins)
+main_AED, bins_AED = np.histogram(np.linalg.eig(main)[0], bins=bins)
 cross_AED, bins_cross = np.histogram(np.linalg.eig(cross)[0], bins=bins)
 total_AED, bins_total = np.histogram(np.linalg.eig(total)[0], bins=bins)
 
@@ -36,10 +38,12 @@ for i in range(average):
     HH = H @ np.conj(H.T)
     total = GH + HG + HH + GG
     cross = GH + HG
+    main = HH + GG
     GG_AED += np.histogram(np.linalg.eig(GG)[0], bins=bins)[0]
     HH_AED += np.histogram(np.linalg.eig(HH)[0], bins=bins)[0]
     cross_AED += np.histogram(np.linalg.eig(cross)[0], bins=bins)[0]
     total_AED += np.histogram(np.linalg.eig(total)[0], bins=bins)[0]
+    main_AED += np.histogram(np.linalg.eig(main)[0], bins=bins)[0]
 
 fig, ax = plt.subplots()
 plt.title("AED Comparison: i.i.d, $\mathbb{N}(0,1/N)$, H = H_1 \Phi H_2")
@@ -52,6 +56,9 @@ ax.plot(abs(bins_hh[1::]), AED, label='$HH^H$ (IRS Path)')
 
 AED = cross_AED/(cols*average)
 ax.plot(bins_cross[1::], AED, label='$GH^H + HG^H$')
+
+AED = main_AED/(cols*average)
+ax.plot(bins_total[1::], AED, label='$GG^H+ HH^H$')
 
 AED = total_AED/(cols*average)
 ax.plot(bins_total[1::], AED, label='$(G + H)(G+H)^H$')
