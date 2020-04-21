corrected variance error on channels

simulation/src/circle_laws.py
@@ -8,7 +8,8 @@ delta = lambda x: x == 0
 estimated_pdf = lambda x: 1/np.pi * np.imag(x)
 
 
-
+# Quarter Circle Related
+st_quarter_circle_eigen = lambda s: 1/2*np.sqrt(1-4/s) - 1/2
 st_quarter_circle = lambda s: 2*np.sqrt(4-np.power(s, 2))/np.pi * np.log((2+np.sqrt(4-np.power(s, 2)))/(-s))-s/2-2/np.pi
 marcenko = lambda x, beta: pos(1-1/beta)*delta(x) + \
                            np.sqrt(pos(x-np.power((1-np.sqrt(beta)), 2))*pos(np.power((1+np.sqrt(beta)), 2)-x)
@@ -19,7 +20,6 @@ marcenko = lambda x, beta: pos(1-1/beta)*delta(x) + \
 
 def st_half_circle(s):
     return (s / 2) * np.sqrt(1 - 4 / np.power(s, 2)) - s / 2
-
 
 def semi_cicle_aed(x):
     return 1 / (2 * np.pi)*np.sqrt(4-np.power(x, 2))
@@ -37,6 +37,7 @@ def half_circle_stieltjes_gamma(s_vector, iterations):
 
 
 def deformed_qc_eigen(x, alpha):
+    #TODO don't allow nan
     interval = np.logical_and(np.power(1-np.sqrt(alpha), 2) < x, x < np.power(1+np.sqrt(alpha), 2))
     output = interval*np.sqrt((x-np.power(1-np.sqrt(alpha), 2))*(np.power(1+np.sqrt(alpha), 2)-x))/(2*np.pi*alpha*x)
     nan = np.isnan(output)

simulation/src/lin_alg.py
@@ -6,10 +6,12 @@ capacity = lambda matrix: np.sum(np.log(1 + np.linalg.eigvalsh(matrix)))
 
 def aed_capacity(value, value_probability, step, snr, number_receivers):
     probabilities = value_probability*step
+    check = np.sum(probabilities)
+    check1 = np.sum(np.log(1 + snr*value)*probabilities)
     return number_receivers*np.sum(np.log(1 + snr*value)*probabilities)
 
 
-c_rand = lambda rows, cols:  (np.random.randn(rows, cols) + 1j*np.random.randn(rows, cols))/np.sqrt(rows)
+c_rand = lambda rows, cols:  (np.random.randn(rows, cols) + 1j*np.random.randn(rows, cols))/np.sqrt(2*rows)
 # c_rand = lambda rows, cols: -1j*1j*np.eye(rows)
 
 random_phase = lambda size: np.diag(np.exp(1j * np.random.uniform(0, 2 * np.pi, size)))