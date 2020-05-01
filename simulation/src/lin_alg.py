Corrected normalization details on numerical resuls for presentation

simulation/src/lin_alg.py
@@ -11,7 +11,12 @@ def aed_capacity(x_value, value_probability, step, snr, number_receivers):
     return number_receivers*np.sum(np.log2(1 + snr*x_value)*probabilities)
 
 
-c_rand = lambda rows, cols: (np.random.randn(rows, cols) + 1j*np.random.randn(rows, cols))/np.sqrt(2*rows)
+def c_rand(rows, cols, var=None):
+    if var is not None:
+        return (np.random.randn(rows, cols) + 1j*np.random.randn(rows, cols))*np.sqrt(var)
+    else:
+        return (np.random.randn(rows, cols) + 1j*np.random.randn(rows, cols))/np.sqrt(2*rows)
+
 c_rand_mean = lambda rows, cols: 1e-1 + 1j*1e-1 + (np.random.randn(rows, cols) + 1j*np.random.randn(rows, cols))/np.sqrt(2*rows)
 
 c_rand_2 = lambda rows, cols:  (np.random.randn(rows, cols) + 1j*np.random.randn(rows, cols))/np.sqrt(cols*rows)

simulation/testing/channel_testing/increasing_antennas.py
@@ -17,7 +17,9 @@ phi = 1    # phi=1 is full rank line of sight.  L/T
 
 numeric_capacity_list = []
 water_fill_capacity = []
-param_list = [100, 200, 300, 400, 500]
+G_fill_capacity = []
+H_fill_capacity = []
+param_list = [1, 10, 50, 100, 200, 300]
 
 
 for val in param_list:
@@ -26,11 +28,13 @@ for val in param_list:
     irs = []
     irs_svd = []
     water_capacity = []
+    H_capacity = []
+    G_capacity = []
     capacities = []
     cross_sum = 0
     main_sum = 0
     los_rank_matrix = reduced_rank(rows, phi)
-    average = 3
+    average = 5
     for i in range(average):
         # H = sigma*c_rand(rows, cols) @random_phase(rows)@ c_rand(rows, cols)
         H = sigma*c_rand(rows, cols) @ c_rand(rows, cols)
@@ -56,10 +60,20 @@ for val in param_list:
         capacities.append(capacity(total_irs, 1/cols))
         water_capacity.append(capacity_water_filled(total_channel@Q_x@hermetian(total_channel)))
 
+        Q_H = water_filling(H, 1)
+        Q_G = water_filling(G, 1)
+        H_capacity.append(capacity_water_filled(H@Q_H@hermetian(H)))
+        G_capacity.append(capacity_water_filled(G@Q_G@hermetian(G)))
+
+
+
+
 
 #   Compare Estimated to True Capacity
     numeric_capacity_list.append(np.average(capacities))
     water_fill_capacity.append(np.average(water_capacity))
+    G_fill_capacity.append(np.average(G_capacity))
+    H_fill_capacity.append(np.average(H_capacity))
 
 #   Plot results
 fig, ax = plt.subplots()
@@ -67,6 +81,9 @@ plt.title("Capacity Vs. Num Antennas")
 # plt.title("Capacity Vs. IRS Path Attenuation")
 ax.plot(param_list, numeric_capacity_list, label='equal power', c='r')
 ax.plot(param_list, water_fill_capacity, label='water filled capacity', c='g')
+ax.plot(param_list, H_fill_capacity, label='IRS channel')
+ax.plot(param_list, G_fill_capacity, label='LOS channel')
+
 ax.set_ylabel('Capacity (Bits)')
 # ax.set_xlabel('Relative Attenuation Coefficient')
 ax.set_xlabel('Number Antennas')

simulation/testing/phase_optimization/phase_check_simulation.py
@@ -5,8 +5,8 @@ from src.lin_alg import *
 import seaborn
 
 size = 100
-n_t = 4
-n_r = 4
+n_t = 100
+n_r = 100
 rows = size
 cols = size
 bins = int(size/2)
@@ -17,10 +17,12 @@ cross_sum = 0
 main_sum = 0
 
 average = 1000
-H_1 = c_rand(size, n_t)
-H_2 = c_rand(n_r, size)
+# H_1 = c_rand(size, n_t)
+H_1 = c_rand(size, n_t, var=1/np.sqrt(n_r*size))
+H_2 = c_rand(n_r, size, var=1/np.sqrt(n_r*size))
+# H_1 = c_rand(size, n_t)
+# H_2 = c_rand(n_r, size)
 G = c_rand(n_r, n_t)
-F = c_rand(rows, cols)
 for i in range(average):
     H = H_2 @ np.diag(np.exp(1j * np.random.uniform(0, 2 * np.pi, size))) @ H_1
     # F = c_rand(rows, cols) @ np.diag(np.exp(1j * np.random.uniform(0, 2 * np.pi, rows))) @ c_rand(rows, cols)